e2aeddb2132119587e848aff535d75b6fe84a46c
Reja El <rejael09@gmail.com>
2023-08-12 11:20:56 +0800

    first init github

---
+++ assets/js/word.js
@@ -0,0 +1,50 @@
+const wordList = [
+  {
+    word: "jokowi",
+    hint: "Siapa Presiden Indonesia Sekarang?"
+  },
+  {
+    word: "jakarta",
+    hint: "Dimana Ibukota Indonesia?"
+  },
+  {
+    word: "agustus",
+    hint: "BUlan kemerdekaan Indonesia?"
+  },
+  {
+    word: "soekarno",
+    hint: "Siapa proklamator?"
+  },
+  {
+    word: "hatta",
+    hint: "Siapa wakil proklamator?"
+  },
+  {
+    word: "belanda",
+    hint: "Perang kemerdekaan melawan?"
+  },
+  {
+    word: "soekarno",
+    hint: "Siapa penulis teks proklamasi?"
+  },
+  {
+    word: "merdeka",
+    hint: "Nama istana pertama?"
+  },
+  {
+    word: "soekarno",
+    hint: "Revolusi mental dirintis?"
+  },
+  {
+    word: "jakarta",
+    hint: "Proklamasi dibacakan di?"
+  },
+  {
+    word: "kartini",
+    hint: "Tokoh perjuangan wanita?"
+  },
+  {
+    word: "diponegoro",
+    hint: "Pangeran yang melawan kolonial?"
+  },
+];
